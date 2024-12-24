1d987cf58da89b0b4dc18ed001f8ac1c4b559b86
JuggerPika <vanben2005@gmail.com>
2024-12-24 13:49:13 +0200

    add wave and menu with exit and invetory

--- main.py
+++ main.py
@@ -21,13 +21,49 @@ nameHero = input('Enter name your hero: ')
 
 # after kill enemy you might, take loot, next wawe, check backpage, check
 hpHero = 10
-hpEnemy = 3
+invetory = {}
+
+
+numberEnemy = {
+	'first': 3, 
+	'second': 10, 
+	'three': 3,
+	'forth': 5,
+	'fiveth': 5,
+}
 wave = 0
+maxWave = 1
+# print(numberEnemy)
+
+while wave < maxWave:
+	print('wave', wave)
+	attackHero = random.randint(1,2)
+	attackEnemy = random.randint(1,2)
+
+	print('Menu')
+	print('1 next wave')
+	print('2 check invetory')
+	print('3 exit game')
+
+	menu = int(input('Select answer: '))
+
+	if menu == 3:
+		wave = maxWave
+		print('Quit the game, bye!')
+	elif menu == 2:
+		print(invetory)
+	if wave == 0 and menu == 1:
+		enemy = numberEnemy.get('first')
+		while enemy > 0:
+			enemy = enemy - attackHero
+			print('hp 1', enemy)
+			print('attack', attackHero)
+
+		wave += 1
+		print('wave', wave)
 
 
-attackHero = random.randint(3,5)
-attackEnemy = random.randint(1,2)
-
-while wave < 5:
 	
-	numberEnemy = numberEnemy + 1
+
+
+
